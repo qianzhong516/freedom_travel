c0c52b0e96a9d52c38576f9d4f2a9671405b0930
Janice <zhongqian516@gmail.com>
2021-04-10 23:57:46 +1000

    added production baseurl

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? "" : "http://192.168.1.88:8000"
+    baseURL: process.env.NODE_ENV === "production" ? "https://freedom-travel-backend.herokuapp.com" : "http://192.168.1.88:8000"
 })
 
 export default instance
